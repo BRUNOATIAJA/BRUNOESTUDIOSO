a832701f656b60fcfeec92d915088a3b4e1171f0
BRUNOATIAJA <batiaja@alumno.montepiedra.edu.ec>
2025-10-21 17:01:04 -0500

    Leer y mostrar el mayor.cpp

---
+++ Leer y mostrar el mayor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Ingresa dos números: ";
+    cin >> a >> b;
+    cout << "Mayor: " << (a > b ? a : b);
+    return 0;
+}
