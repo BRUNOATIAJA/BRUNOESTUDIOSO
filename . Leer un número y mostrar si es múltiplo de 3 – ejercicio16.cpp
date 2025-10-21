304f2435d46653c375887083f0918b29afb0f6a6
BRUNOATIAJA <batiaja@alumno.montepiedra.edu.ec>
2025-10-21 17:13:42 -0500

    Leer un número y mostrar si es múltiplo de 3 – ejercicio16.cpp

---
+++ . Leer un número y mostrar si es múltiplo de 3 – ejercicio16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 3 == 0)
+        cout << "Es múltiplo de 3";
+    else
+        cout << "No es múltiplo de 3";
+    return 0;
+}
