6899c9535e219c4bd2f3c7ffe7d4506fb75b685e
BRUNOATIAJA <batiaja@alumno.montepiedra.edu.ec>
2025-10-27 13:19:49 -0500

    2. resta_numeros.cpp

---
+++ 🧩 2. resta_numeros.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Ingrese dos números: ";
+    cin >> a >> b;
+    cout << "Resta = " << a - b << endl;
+    return 0;
+}
