db2648ade954397e4a47270036d501dc50a5c728
BRUNOATIAJA <batiaja@alumno.montepiedra.edu.ec>
2025-10-27 13:32:18 -0500

    10. suma_pares_hasta_n.cpp

---
+++ 10. suma_pares_hasta_n.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, suma = 0;
+    cout << "Ingrese un número: ";
+    cin >> n;
+    for (int i = 2; i <= n; i += 2) suma += i;
+    cout << "Suma de pares = " << suma;
+    return 0;
+}
