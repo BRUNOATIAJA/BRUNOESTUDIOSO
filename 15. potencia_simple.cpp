8ccb272fd3945bf880e1d09d9f5dea96b5b50aba
BRUNOATIAJA <batiaja@alumno.montepiedra.edu.ec>
2025-10-27 13:54:06 -0500

    15. potencia_simple.cpp

---
+++ 15. potencia_simple.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double base, exponente;
+    cout << "Base y exponente: ";
+    cin >> base >> exponente;
+    cout << "Resultado = " << pow(base, exponente);
+    return 0;
+}
