9a5336aaada164e7a0033cad8e87387f211eba06
BRUNOATIAJA <batiaja@alumno.montepiedra.edu.ec>
2025-10-27 13:22:15 -0500

    7. promedio_tres.cpp

---
+++ 7. promedio_tres.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cout << "Ingrese tres números: ";
+    cin >> a >> b >> c;
+    cout << "Promedio = " << (a + b + c) / 3;
+    return 0;
+}
